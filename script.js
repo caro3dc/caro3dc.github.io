99328fdd31ba0fb068aa2311946c567b6b87338b
caro3dc <caro3dc@gmail.com>
2022-01-24 18:38:40 +0800

    2021 0124 1837: final?

---
+++ script.js
@@ -0,0 +1,39 @@
+function home() {
+  document.getElementById("home").style.display = "inline";
+  document.getElementById("info").style.display = "none";
+  document.getElementById("outline").style.display = "none";
+  document.getElementById("promo").style.display = "none";
+  document.getElementById("about").style.display = "none";
+}
+
+function info() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("info").style.display = "inline";
+  document.getElementById("outline").style.display = "none";
+  document.getElementById("promo").style.display = "none";
+  document.getElementById("about").style.display = "none";
+}
+
+function outline() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("info").style.display = "none";
+  document.getElementById("outline").style.display = "inline";
+  document.getElementById("promo").style.display = "none";
+  document.getElementById("about").style.display = "none";
+}
+
+function promo() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("info").style.display = "none";
+  document.getElementById("outline").style.display = "none";
+  document.getElementById("promo").style.display = "inline";
+  document.getElementById("about").style.display = "none";
+}
+
+function about() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("info").style.display = "none";
+  document.getElementById("outline").style.display = "none";
+  document.getElementById("promo").style.display = "none";
+  document.getElementById("about").style.display = "inline";
+}
