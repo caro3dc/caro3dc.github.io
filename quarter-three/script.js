8a4a8252d2637a216e2e3be930c2b2423a5b6bcf
caro3dc <ap.carlosromeoiii.delcastillo@apecschools.edu.ph>
2022-05-09 13:40:40 +0800

    2022 0509 1340: eyo

---
+++ quarter-three/script.js
@@ -0,0 +1,193 @@
+function home() {
+  document.getElementById("home").style.display = "inline";
+  document.getElementById("info").style.display = "none";
+  document.getElementById("outline").style.display = "none";
+  document.getElementById("promo").style.display = "none";
+  document.getElementById("about").style.display = "none";
+}
+
+function info() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("info").style.display = "inline";
+  document.getElementById("outline").style.display = "none";
+  document.getElementById("promo").style.display = "none";
+  document.getElementById("about").style.display = "none";
+}
+
+function outline() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("info").style.display = "none";
+  document.getElementById("outline").style.display = "inline";
+  document.getElementById("promo").style.display = "none";
+  document.getElementById("about").style.display = "none";
+}
+
+function promo() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("info").style.display = "none";
+  document.getElementById("outline").style.display = "none";
+  document.getElementById("promo").style.display = "inline";
+  document.getElementById("about").style.display = "none";
+}
+
+function about() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("info").style.display = "none";
+  document.getElementById("outline").style.display = "none";
+  document.getElementById("promo").style.display = "none";
+  document.getElementById("about").style.display = "inline";
+}
+
+function anho() {
+  document.getElementById("anho").style.display = "inline";
+  document.getElementById("abst").style.display = "none";
+  document.getElementById("up").style.display = "none";
+  document.getElementById("down").style.display = "none";
+  document.getElementById("down2").style.display = "none";
+  document.getElementById("auld").style.display = "none";
+  document.getElementById("auld2").style.display = "none";
+  document.getElementById("vweg").style.display = "none";
+  document.getElementById("vweg2").style.display = "none";
+  document.getElementById("ind").style.display = "none";
+  document.getElementById("baxo").style.display = "none";
+}
+
+function abst() {
+  document.getElementById("anho").style.display = "none";
+  document.getElementById("abst").style.display = "inline";
+  document.getElementById("up").style.display = "none";
+  document.getElementById("down").style.display = "none";
+  document.getElementById("down2").style.display = "none";
+  document.getElementById("auld").style.display = "none";
+  document.getElementById("auld2").style.display = "none";
+  document.getElementById("vweg").style.display = "none";
+  document.getElementById("vweg2").style.display = "none";
+  document.getElementById("ind").style.display = "none";
+  document.getElementById("baxo").style.display = "none";
+}
+
+function up() {
+  document.getElementById("anho").style.display = "none";
+  document.getElementById("abst").style.display = "none";
+  document.getElementById("up").style.display = "inline";
+  document.getElementById("down").style.display = "none";
+  document.getElementById("down2").style.display = "none";
+  document.getElementById("auld").style.display = "none";
+  document.getElementById("auld2").style.display = "none";
+  document.getElementById("vweg").style.display = "none";
+  document.getElementById("vweg2").style.display = "none";
+  document.getElementById("ind").style.display = "none";
+  document.getElementById("baxo").style.display = "none";
+}
+
+function down() {
+  document.getElementById("anho").style.display = "none";
+  document.getElementById("abst").style.display = "none";
+  document.getElementById("up").style.display = "none";
+  document.getElementById("down").style.display = "inline";
+  document.getElementById("down2").style.display = "none";
+  document.getElementById("auld").style.display = "none";
+  document.getElementById("auld2").style.display = "none";
+  document.getElementById("vweg").style.display = "none";
+  document.getElementById("vweg2").style.display = "none";
+  document.getElementById("ind").style.display = "none";
+  document.getElementById("baxo").style.display = "none";
+}
+
+function down2() {
+  document.getElementById("anho").style.display = "none";
+  document.getElementById("abst").style.display = "none";
+  document.getElementById("up").style.display = "none";
+  document.getElementById("down").style.display = "none";
+  document.getElementById("down2").style.display = "inline";
+  document.getElementById("auld").style.display = "none";
+  document.getElementById("auld2").style.display = "none";
+  document.getElementById("vweg").style.display = "none";
+  document.getElementById("vweg2").style.display = "none";
+  document.getElementById("ind").style.display = "none";
+  document.getElementById("baxo").style.display = "none";
+}
+
+function auld() {
+  document.getElementById("anho").style.display = "none";
+  document.getElementById("abst").style.display = "none";
+  document.getElementById("up").style.display = "none";
+  document.getElementById("down").style.display = "none";
+  document.getElementById("down2").style.display = "none";
+  document.getElementById("auld").style.display = "inline";
+  document.getElementById("auld2").style.display = "none";
+  document.getElementById("vweg").style.display = "none";
+  document.getElementById("vweg2").style.display = "none";
+  document.getElementById("ind").style.display = "none";
+  document.getElementById("baxo").style.display = "none";
+}
+
+function auld2() {
+  document.getElementById("anho").style.display = "none";
+  document.getElementById("abst").style.display = "none";
+  document.getElementById("up").style.display = "none";
+  document.getElementById("down").style.display = "none";
+  document.getElementById("down2").style.display = "none";
+  document.getElementById("auld").style.display = "none";
+  document.getElementById("auld2").style.display = "inline";
+  document.getElementById("vweg").style.display = "none";
+  document.getElementById("vweg2").style.display = "none";
+  document.getElementById("ind").style.display = "none";
+  document.getElementById("baxo").style.display = "none";
+}
+
+function vweg() {
+  document.getElementById("anho").style.display = "none";
+  document.getElementById("abst").style.display = "none";
+  document.getElementById("up").style.display = "none";
+  document.getElementById("down").style.display = "none";
+  document.getElementById("down2").style.display = "none";
+  document.getElementById("auld").style.display = "none";
+  document.getElementById("auld2").style.display = "none";
+  document.getElementById("vweg").style.display = "inline";
+  document.getElementById("vweg2").style.display = "none";
+  document.getElementById("ind").style.display = "none";
+  document.getElementById("baxo").style.display = "none";
+}
+
+function vweg2() {
+  document.getElementById("anho").style.display = "none";
+  document.getElementById("abst").style.display = "none";
+  document.getElementById("up").style.display = "none";
+  document.getElementById("down").style.display = "none";
+  document.getElementById("down2").style.display = "none";
+  document.getElementById("auld").style.display = "none";
+  document.getElementById("auld2").style.display = "none";
+  document.getElementById("vweg").style.display = "none";
+  document.getElementById("vweg2").style.display = "inline";
+  document.getElementById("ind").style.display = "none";
+  document.getElementById("baxo").style.display = "none";
+}
+
+function ind() {
+  document.getElementById("anho").style.display = "none";
+  document.getElementById("abst").style.display = "none";
+  document.getElementById("up").style.display = "none";
+  document.getElementById("down").style.display = "none";
+  document.getElementById("down2").style.display = "none";
+  document.getElementById("auld").style.display = "none";
+  document.getElementById("auld2").style.display = "none";
+  document.getElementById("vweg").style.display = "none";
+  document.getElementById("vweg2").style.display = "none";
+  document.getElementById("ind").style.display = "inline";
+  document.getElementById("baxo").style.display = "none";
+}
+
+function baxo() {
+  document.getElementById("anho").style.display = "none";
+  document.getElementById("abst").style.display = "none";
+  document.getElementById("up").style.display = "none";
+  document.getElementById("down").style.display = "none";
+  document.getElementById("down2").style.display = "none";
+  document.getElementById("auld").style.display = "none";
+  document.getElementById("auld2").style.display = "none";
+  document.getElementById("vweg").style.display = "none";
+  document.getElementById("vweg2").style.display = "none";
+  document.getElementById("ind").style.display = "none";
+  document.getElementById("baxo").style.display = "inline";
+}
